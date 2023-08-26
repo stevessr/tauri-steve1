e71d2bd86726e60577d6be39df7a9a5835463222
stevessr <89645372+stevessr@users.noreply.github.com>
2023-08-26 15:52:51 +0800

    1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+mod menu;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +8,10 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let context = tauri::generate_context!();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .menu(menu::init(&context))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
